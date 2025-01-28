e38895c2378aaa1fc5c5b08cf94a45de889930b7
Andres7283 <agarcia7283@ceuandalucia.es>
2025-01-28 18:04:48 +0100

    introduccion de html

---
+++ Tema5/src/main/java/com/ej15/Tema4E151Application.java
@@ -0,0 +1,13 @@
+package com.ej15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tema4E151Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tema4E151Application.class, args);
+	}
+
+}
