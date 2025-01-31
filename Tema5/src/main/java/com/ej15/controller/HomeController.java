9301c8b53e49bbec4c87849048f80e39b90c546a
Andres7283 <agarcia7283@ceuandalucia.es>
2025-01-31 16:57:21 +0100

    hacer ejarcicio 16 y 17 tema 4 y ejercicio 1 tama 5

--- Tema5/src/main/java/com/ej15/controller/HomeController.java
+++ Tema5/src/main/java/com/ej15/controller/HomeController.java
@@ -1,8 +1,15 @@
 package com.ej15.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ej15.model.User;
 
 @Controller
 public class HomeController {
@@ -13,7 +20,35 @@ public class HomeController {
 		model.addAttribute("nombre1", "Andrés Avelino García Romero");
 		model.addAttribute("nombre2", "<b> Andrés Avelino García Romero </b>");
 		model.addAttribute("imagen", "/imagenes/pizza.jpg");
+		model.addAttribute("role", "admin");
+		
+		List<String> personas = new ArrayList<>();
+		personas.add("Belen");
+		personas.add("Andres");
+		personas.add("Tay");
+		personas.add("Laura");
+		personas.add("Vega");
+		model.addAttribute("persona", personas);
+		
+		model.addAttribute("submit", "/form");
 		return "home";
 	}
 		
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/form")
+	public String form(Model model) {
+			model.addAttribute("user", new User());
+		return "form";
+	}
+	
+
+	@PostMapping("/guardar")
+	public String guardar(@ModelAttribute User user, Model model) {
+		model.addAttribute("user", user);
+		return "result";
+	}
 }
---
+++ Tema5/src/main/java/com/ej15/model/User.java
@@ -0,0 +1,25 @@
+package com.ej15.model;
+
+public class User {
+	
+	private String nombre;
+	private int edad;
+	
+	public User() {
+		super();
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+}
